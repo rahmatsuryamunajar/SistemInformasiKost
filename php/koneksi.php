b7eed1aed2b9b0e29e45777744678640ccdc13c0
rahmatsuryamunajar <12050112380@students.uin-suska.ac.id>
2022-06-29 16:41:01 +0700

    first commit

---
+++ php/koneksi.php
@@ -0,0 +1,8 @@
+<?php
+$koneksi = mysqli_connect("localhost", "root", "", "sisteminformasikost");
+
+// if (!$koneksi) {
+//     echo "gagal terhubung ke database";
+// } else {
+//     echo "terhubung";
+// }
---
+++ template/footer.php
@@ -0,0 +1,14 @@
+</div>
+<style>
+    .copyright {
+        padding: 50px;
+    }
+</style>
+<footer style="margin-top:100px;color:white" class="sticky-footer bg-danger">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Rahmat Surya Munajar | Kost pekanbaru</span>
+        </div>
+    </div>
+</footer>
+</body>
